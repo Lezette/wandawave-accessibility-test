452f1b238252327c8124dd68460d5137e465a581
elizabeth <leezeelola@gmail.com>
2023-11-13 16:00:06 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const menuBtn = document.getElementById("menuBtn");
+const menuIcon = document.getElementById("menuIcon");
+const close = document.getElementById("close");
+const menu = document.getElementById("menu");
+const header = document.getElementById("header");
+
+menuBtn.addEventListener("click", () => {
+  if (menuIcon.classList.contains("hidden")) {
+    menuIcon.classList.remove("hidden");
+    close.classList.add("hidden");
+    menu.classList.add("hidden");
+    header.classList.add("absolute");
+  } else {
+    menuIcon.classList.add("hidden");
+    close.classList.remove("hidden");
+    menu.classList.remove("hidden");
+    header.classList.remove("absolute");
+  }
+});
