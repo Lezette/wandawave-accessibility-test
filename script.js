4d2033c7f367d14962d6d8f63e5b1799e09a8579
elizabeth <leezeelola@gmail.com>
2023-11-13 16:20:11 +0100

    lightroom test fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menuBtn = document.getElementById("menuBtn");
 const menuIcon = document.getElementById("menuIcon");
-const close = document.getElementById("close");
+const close = document.getElementById("close-menu");
 const menu = document.getElementById("menu");
 const header = document.getElementById("header");
 
